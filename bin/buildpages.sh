3fd6beb919886067c0ec0baa34dcf32e65547c4b
Dan Carroll <dan@carroll.id.au>
2023-04-12 16:20:43 +1000

    correct path to requirements file

--- bin/buildpages.sh
+++ bin/buildpages.sh
@@ -4,7 +4,7 @@ set -x
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-pip
 
-pip3 install -v -r requirements.txt 
+pip3 install -v -r docs/requirements.txt 
 
 cd docs
 pwd ls -lah
